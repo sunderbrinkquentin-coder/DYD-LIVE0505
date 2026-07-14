a55e82695f93dfc2d23431c3569aafe8265e7659
sunderbrinkquentin-coder <sunderbrinkquentin@gmail.com>
2026-07-14 16:51:56 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
 
 const rootElement = document.getElementById('root');
 
